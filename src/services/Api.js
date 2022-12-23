19c5a8ef9b82c5d10100f4b25673e7a44d5553a7
Lietson dos santos <66736104+lietsondossanto@users.noreply.github.com>
2022-12-23 11:29:03 +0100

    Delete src directory

--- src/services/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({
-  baseURL: "https://api.ipdata.co/",
-  method: "GET",
-  headers: { Accept: "application/json" },
-});
-
-export default Api;
